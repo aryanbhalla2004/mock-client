b62c3505e80d2d2ea1bfaf9c2470188724e3b164
Aryan Bhalla <aryanbhalla66@gmail.com>
2023-07-08 00:52:19 -0500

    almost finxihed coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import "./App.css";
+import logo from "./assets/logo.png";
 
 const App = () => {
+
+  const handleClick = () => {
+    window.location.href = 'https://crew.zoobo.ca';
+  }
+
+  
   return (
-    <div>App</div>
+    <div className='main-container-coming-soon'>
+      <img src={logo}/>
+      <div className=''>
+        <h1>Pardon our dust, our website is currently under construction.</h1>
+        <p className='mt-2'>Apologies for any inconvenience caused. Our website is currently undergoing enhancements to provide you with an even better browsing experience. We appreciate your patience and invite you to check back soon.</p>
+        <p>Exciting news! We're also actively hiring and would love to have you join our team. Visit our crew site now to explore exciting job opportunities and kick-start your journey with us.</p>
+        <button onClick={handleClick}>Join Our Team <i className="bi bi-arrow-right"></i></button>
+      </div>
+      <footer>
+
+      </footer>
+    </div>
   )
 }
 
